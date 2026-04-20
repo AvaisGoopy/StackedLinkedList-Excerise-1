6fdca92bba58957c3727b1d7e893ce13f8d8ff45
20391420 <20391420@MS-12-14-HPE>
2026-04-20 11:36:33 +0100

    TIdied up code

--- StackedLinkedList Excerise 1/IMember.cs
+++ StackedLinkedList Excerise 1/IMember.cs
@@ -6,32 +6,8 @@ using System.Threading.Tasks;
 
 namespace StackedLinkedList_Excerise_1
 {
-    //internal interface IMember
-    //{
-    //    /// <summary>
-    //    /// Adds value to list appropriately for a stack
-    //    /// </summary>
-    //    /// <param name="value"></param>
-    //    public void Push(int value);
-
-    //    public int Pop();
-
-    //    public bool IsEmpty();
-
-    //    int Size();
-
-    //    public object Peek();
-
-    //}
-
      interface IMember
-    {
-        /// <summary>
-        /// Adds value to list appropriately for a stack Push(string value)
-        /// <paramref name="value"/>
-        /// </summary>
-       
-       
+    {     
         public void Push(string value);
 
 
